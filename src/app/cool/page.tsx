1e984bbc6b488f7105e58dbcf08eedfda53a5800
asharif34 <asharif34@gatech.edu>
2025-01-15 23:26:29 -0500

    Update cool page

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,13 +1,22 @@
+import Image from "next/image";
+
 export default function Kewl() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
-      Write something unique about you here! 
-      It could be a club you're part of, a weird skill you have, or something special that happened to you.
-      Feel free to put links, images, whatever! 
-      Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
-      */}
-      😎😎😎
+      <div className="relative">
+        <Image
+          src="/ferrari sf23 transparent.png"
+          alt="Ferrari SF23"
+          width={400}
+          height={300}
+          className="rounded-md shadow-lg"
+        />
+      </div>
+
+      <p className="text-center max-w-md">
+        I love Formula One! I really enjoy watching races & keeping up with the seasonal drama
+        that happens across all the teams - but my favorite team is Ferrari!
+      </p>
     </div>
   );
 }
